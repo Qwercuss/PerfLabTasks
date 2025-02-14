8412dec169bb1753fedae40f745ddc01f89d1215
Qwercuss <52700692+Qwercuss@users.noreply.github.com>
2025-02-14 18:35:00 +0300

    Add files via upload

--- task4/Task_4.cpp
+++ task4/Task_4.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -25,13 +26,25 @@ int main(int argc, char* argv[])
 	
 	in.close();
 	
+	sort(nums.begin(), nums.end());
 
+	if (nums.size() % 2 == 0)
+	{
+		m = (nums[nums.size() / 2 - 1] + nums[nums.size() / 2]) / 2;
+	}
+	else
+	{
+		m = nums[nums.size() / 2];
+	}
+
+	/*
 	for (int i = 0; i < nums.size(); i++)
 	{
 		m = m + nums[i];
 	}
 
 	m = m / nums.size(); // целевое число
+	*/
 
 	for (int i = 0; i < nums.size(); i++)
 	{
