35727d5fa9bffb7c1a35e65dda3fee1fc17e2e82
Qwercuss <52700692+Qwercuss@users.noreply.github.com>
2025-02-14 14:13:31 +0300

    Add files via upload

---
+++ task4/Task_4.cpp
@@ -0,0 +1,44 @@
+﻿
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	vector <int> nums;
+
+	int m = 0;
+	int p = 0;
+	int n;
+
+	ifstream in(argv[1]);
+
+	if (in.is_open())
+	{
+		while (in>>n)
+		{
+			nums.push_back(n);
+		}
+	}
+	
+	in.close();
+	
+
+	for (int i = 0; i < nums.size(); i++)
+	{
+		m = m + nums[i];
+	}
+
+	m = m / nums.size(); // целевое число
+
+	for (int i = 0; i < nums.size(); i++)
+	{
+		p = p + abs(m - nums[i]);
+	}
+
+	cout << p;
+
+	return 0;
+}
